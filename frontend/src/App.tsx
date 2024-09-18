fe086dda65b038cbffca4b4700a6a98fe0fcc497
bril95 <bril.vadim@mail.ru>
2024-09-18 11:07:25 +0300

    add material UI

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,22 +1,13 @@
-import React from 'react';
-import './App.css';
+import Button from '@mui/material/Button';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src="./logo.svg" className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Welcome to My App</h1>
+        <Button variant="contained" color="primary">
+          Material UI Button
+        </Button>
       </header>
     </div>
   );
