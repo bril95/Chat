25c20a10203408c023ed1d229f8093f67b226156
bril95 <bril.vadim@mail.ru>
2024-09-20 13:24:44 +0300

    update packege.json, add locales

---
+++ frontend/src/internalization/locales/en.js
@@ -0,0 +1,80 @@
+const en = {
+  translation: {
+    headers: {
+      hexletChat: 'Hexlet Chat',
+    },
+    loginPage: {
+      enter: 'Login',
+      username: 'Your nickname',
+      password: 'Password',
+      withoutAccount: 'Don\'t have an account?',
+      registration: 'Sign Up',
+      error: 'Invalid username or password',
+      isLoading: 'Loading...',
+    },
+    chatMainPage: {
+      exit: 'Logout',
+      channels: 'Channels',
+      placeholderMessage: 'Enter a message...',
+      toastError: 'Connection error',
+      plus: '+',
+      messages: {
+        key_zero: '{{count}} messages',
+        key_one: '{{count}} message',
+        key_few: '{{count}} messages',
+        key_many: '{{count}} messages',
+      },
+    },
+    signUpPage: {
+      placeholder: {
+        min3max20: 'From 3 to 20 characters',
+        min6: 'At least 6 characters',
+        samePassword: 'Passwords must match',
+      },
+      errorPassword: 'This user already exists',
+      errorRegistration: 'An error occurred during registration',
+      registration: 'Sign Up',
+      username: 'Username',
+      password: 'Password',
+      confirmPassword: 'Confirm password',
+      signUp: 'Register',
+    },
+    notFoundPage: {
+      pageNotFound: 'Page not found',
+    },
+    modalWindows: {
+      cancel: 'Cancel',
+      submit: 'Submit',
+      channelManagment: 'Channel management',
+      addChannel: {
+        addChannel: 'Add channel',
+        channelName: 'Channel name',
+        toastAddName: 'Channel created',
+        toastErrorAddName: 'Error creating channel',
+      },
+      renameChannel: {
+        rename: 'Rename',
+        renameChannel: 'Rename channel',
+        newName: 'New channel name',
+        toastRenameChannel: 'Channel renamed',
+      },
+      deleteChannel: {
+        deleteChannel: 'Delete channel',
+        confirmation: 'Are you sure?',
+        delete: 'Delete',
+        toastDeleteChannel: 'Channel deleted',
+        toastErrorAddName: 'Error deleting channel',
+      },
+    },
+    schema: {
+      requiredField: 'Required field',
+      min3max20: 'From 3 to 20 characters',
+      min6: 'At least 6 characters',
+      samePassword: 'Passwords must match',
+      sameNameChannel: 'This channel name is already in use',
+      enterNewName: 'Enter a new channel name',
+    },
+  },
+};
+
+export default en;
---
+++ frontend/src/internalization/locales/ru.js
@@ -0,0 +1,81 @@
+
+const ru = {
+  translation: {
+    headers: {
+      hexletChat: 'Hexlet Chat',
+    },
+    loginPage: {
+      enter: 'Войти',
+      username: 'Ваш ник',
+      password: 'Пароль',
+      withoutAccount: 'Нет аккаунта?',
+      registration: 'Регистрация',
+      error: 'Неверные имя пользователя или пароль',
+      isLoading: 'В процессе...',
+    },
+    chatMainPage: {
+      exit: 'Выйти',
+      channels: 'Каналы',
+      placeholderMessage: 'Введите сообщение...',
+      toastError: 'Ошибка соединения',
+      plus: '+',
+      messages: {
+        key_zero: '{{count}} сообщений',
+        key_one: '{{count}} сообщение',
+        key_few: '{{count}} сообщения',
+        key_many: '{{count}} сообщений',
+      },
+    },
+    signUpPage: {
+      placeholder: {
+        min3max20: 'От 3 до 20 символов',
+        min6: 'Не менее 6 символов',
+        samePassword: 'Пароли должны совпадать',
+      },
+      errorPassword: 'Такой пользователь уже существует',
+      errorRegistration: 'Произошла ошибка при регистрации',
+      registration: 'Регистрация',
+      username: 'Имя пользователя',
+      password: 'Пароль',
+      confirmPassword: 'Подтвердите пароль',
+      signUp: 'Зарегистрироваться',
+    },
+    notFoundPage: {
+      pageNotFound: 'Страница не найдена',
+    },
+    modalWindows: {
+      cancel: 'Отмена',
+      submit: 'Отправить',
+      channelManagment: 'Управление каналом',
+      addChannel: {
+        addChannel: 'Добавить канал',
+        channelName: 'Имя канала',
+        toastAddName: 'Канал создан',
+        toastErrorAddName: 'Ошибка при создании канала',
+      },
+      renameChannel: {
+        rename: 'Переименовать',
+        renameChannel: 'Переименовать канал',
+        newName: 'Имя канала',
+        toastRenameChannel: 'Канал переименован',
+      },
+      deleteChannel: {
+        deleteChannel: 'Удалить канал',
+        confirmation: 'Уверены?',
+        delete: 'Удалить',
+        toastDeleteChannel: 'Канал удалён',
+        toastErrorAddName: 'Ошибка при удалении канала',
+      },
+    },
+    schema: {
+      requiredField: 'Обязательное поле',
+      min3max20: 'От 3 до 20 символов',
+      min6: 'Не менее 6 символов',
+      samePassword: 'Пароли должны совпадать',
+      sameNameChannel: 'Это имя канала уже используется',
+      enterNewName: 'Введите новое имя канала',
+    },
+  },
+};
+
+export default ru;
