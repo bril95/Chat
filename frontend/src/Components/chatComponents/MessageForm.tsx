ddecd1f58db710fe7b719beec65406ec42217c61
bril95 <bril.vadim@mail.ru>
2024-10-02 12:09:24 +0300

    add print messages in MessageForm

--- frontend/src/Components/chatComponents/MessageForm.tsx
+++ frontend/src/Components/chatComponents/MessageForm.tsx
@@ -1,15 +1,26 @@
 import { Box, TextField, IconButton, InputAdornment } from '@mui/material';
 import { useTranslation } from 'react-i18next';
 import SendIcon from '@mui/icons-material/Send';
+import axios from 'axios';
+import userStore from '../../store/userStore';
+import channelStore from '../../store/channelStore';
 
 const MessageForm = () => {
   const { t } = useTranslation();
 
+  const token = userStore((store) => store.token);
+  const username = userStore((store) => store.username);
+  const getCurrentChannel = channelStore((store) => store.currentChannel);
+
   const sendMessage = (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
     const formData = new FormData(event.currentTarget);
-    const newChannel = formData.get('messageForm');
-    console.log(newChannel);
+    const newMessage = { body: formData.get('messageForm'), channelId: getCurrentChannel.id, username: username };
+    axios.post('/api/v1/messages', newMessage, {
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  });
     event.currentTarget.reset()
   };
 
