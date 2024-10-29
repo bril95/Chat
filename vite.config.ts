f08f659bdda4e263ea13a9b50b91ea38bd8cd388
bril95 <bril.vadim@mail.ru>
2024-10-29 22:38:31 +0200

    update vite

--- vite.config.ts
+++ vite.config.ts
@@ -10,7 +10,7 @@ export default defineConfig({
     emptyOutDir: true,
   },
   server: {
-    port: 5000,
+    port: Number(process.env.PORT) || 5000,
     host: true,
     proxy: {
       '/api': {
