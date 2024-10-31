2fc642044b2d64a98ea284708ec3ad07539522f1
bril95 <bril.vadim@mail.ru>
2024-10-31 19:25:20 +0200

    upade NotFoundPage

--- frontend/src/Components/misc/Particles.tsx
+++ frontend/src/Components/misc/Particles.tsx
@@ -1,18 +1,13 @@
 import { useEffect } from 'react';
 
-const Particles = () => {
+const Particles = (): JSX.Element => {
   useEffect(() => {
     if (window.particlesJS) {
       window.particlesJS.load('particles-js', '/particles-config.json');
     }
   }, []);
 
-  return (
-    <div
-      id="particles-js"
-      style={{ width: '100%', height: '100vh', position: 'absolute' }}
-    />
-  );
+  return <div id="particles-js" style={{ width: '100%', height: '100vh', position: 'absolute' }} />;
 };
 
 export default Particles;
